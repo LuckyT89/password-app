7e048f01513c66251109fa988a4d7e61205c7a8d
Tyler <tluckewicz@gmail.com>
2021-12-30 09:50:23 -0500

    Add generate random password functionality

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -6,13 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.scss'],
 })
 export class ContainerComponent implements OnInit {
+  lowerCharacters = 'abcdefghijklmnopqrstuvwxyz';
   constructor() {}
 
   ngOnInit(): void {}
 
   buttonClick(input1: string, input2: string, input3: string) {
-    console.log('input1: ', input1);
-    console.log('input2: ', input2);
-    console.log('input3: ', input3);
+    const userPassword = `${input1}${input2}${input3}`;
+    console.log('userPassword: ', userPassword);
+
+    const characterList = this.lowerCharacters;
+
+    // generate 10 random passwords using characters from the character list
+    for (let i = 0; i < 10; i++) {
+      const randomPassowrd = this.generateRandomPassword(characterList);
+      console.log('randomPassword: ', randomPassowrd);
+    }
+  }
+
+  // input a string of characters and return a single random character from that string
+  selectRandomCharacter(characterList: string): string {
+    return characterList.charAt(
+      Math.floor(Math.random() * characterList.length)
+    );
+  }
+
+  // input a list of characters (just a string), select a random character from the list three times, combine them to make a random password,
+  // then return the random password
+  generateRandomPassword(characterList: string): string {
+    const randomCharacter1 = this.selectRandomCharacter(characterList);
+    const randomCharacter2 = this.selectRandomCharacter(characterList);
+    const randomCharacter3 = this.selectRandomCharacter(characterList);
+    const randomPassword = `${randomCharacter1}${randomCharacter2}${randomCharacter3}`;
+    return randomPassword;
   }
 }
